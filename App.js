f1c8ac519c4d0a15757411d10346edeb1e8ab85e
garouuuu <127393684+garouuuu@users.noreply.github.com>
2023-05-06 14:59:21 +0300

    Added Src=Components , Navigator , screens   .  Components=Eikones.js , Kartela.js , Welcome.js , Navigator= AppNavigator.js , Root.js , Tabs.js , screens=Homescreen.js , Profilescreen.js

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { app } from './src/database/database';
+import { StatusBar } from 'expo-status-bar';
+import { TailwindProvider } from 'tailwindcss-react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Root from './src/Navigator/Root';
+import Welcome from './src/components/Welcome';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <TailwindProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Welcome">
+          <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
+          <Stack.Screen name="Root" component={Root} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </TailwindProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/Kartela.js
@@ -0,0 +1,122 @@
+import { View, Text, TouchableOpacity , Image, StyleSheet } from 'react-native'
+import React from 'react'
+
+const star = require("../../assets/star.png");
+const loc = require("../../assets/location-pin.png");
+const Eikona = require("../../assets/372551.jpg");
+
+const Kartela = ({
+    id, 
+    title,
+    rating,
+    genre,
+    address,
+    short_description,
+    dishes,
+    long,
+    lat,
+}) => {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <Image source={Eikona} style={styles.image} />
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{title}</Text>
+        <View style={styles.ratingContainer}>
+          <Image source={star} style={styles.ratingImage} />
+          <Text style={styles.rating}>
+            <Text style={styles.green}>{rating}</Text>
+            <Text style={styles.genre}>{genre}</Text>
+          </Text>
+        </View>
+        <View style={styles.addressContainer}>
+          <Image source={loc} style={styles.addressImage} />
+          <Text style={styles.address}>Nearby {address}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 300,
+    width: 360,
+    backgroundColor: 'white',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+    marginRight: 3,
+  },
+
+  image: {
+    height: 200,
+    width: 360,
+    borderRadius: 2,
+  },
+
+  textContainer: {
+    padding: 8,
+    paddingHorizontal: 16,
+  },
+
+  title: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    paddingTop: 8,
+  },
+
+  ratingContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 4,
+  },
+
+  ratingImage: {
+    height: 16,
+    width: 16,
+    opacity: 0.5,
+  },
+
+  rating: {
+    fontSize: 12,
+    color: '#717171',
+  },
+
+  green: {
+    color: '#50c878',
+  },
+
+  genre: {
+    fontSize: 12,
+    color: '#717171',
+    marginLeft: 4,
+  },
+
+  addressContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 4,
+  },
+
+  addressImage: {
+    height: 18,
+    width: 18,
+    opacity: 0.4,
+  },
+
+  address: {
+    fontSize: 12,
+    color: '#717171',
+    marginLeft: 4,
+  },
+})
+
+export default Kartela
