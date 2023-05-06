ef86818dcfd4a1fdf329a0429f08f3fb6b3d8921
Mike17K <kaipismike1@gmail.com>
2023-05-06 15:18:35 +0300

    fixed issues of merging

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { app } from './src/database/database';
 import { StatusBar } from 'expo-status-bar';
-import { TailwindProvider } from 'tailwindcss-react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Root from './src/Navigator/Root';
@@ -11,13 +10,11 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <TailwindProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Welcome">
-          <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
-          <Stack.Screen name="Root" component={Root} options={{ headerShown: false }} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </TailwindProvider>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
+        <Stack.Screen name="Root" component={Root} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
